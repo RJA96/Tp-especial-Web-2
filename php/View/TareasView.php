1510659b231692e2a019f62a2a42f85b239000d7
Rodrigo Aguilar <rodrigoaguilar96@gmail.com>
2018-09-11 18:49:22 -0300

    Home totalmente funcional en view

--- php/View/TareasView.php
+++ php/View/TareasView.php
@@ -11,8 +11,30 @@ class TareasView
    ?>
    <!doctype html>
    <html lang="en">
-     <head>
-     </head>
+    <head>
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+     <link rel="stylesheet" href="css/estilos.css">
+
+    </head>
+    <header>
+      <nav class="navbar navbar-dark bg-dark navbar-expand-md">
+        <a class="navbar-brand">PeliWeb</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"> </span>
+        </button>
+        <div class="collapse navbar-collapse">
+          <div class="navbar-nav">
+            <a class="nav-item nav-link active home" href="#">Home<span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link all-movies" href="#">Todas las peliculas</a>
+            <a class="nav-item nav-link sugerencias" href="#">Sugerencias</a>
+            <a class="nav-item nav-link contacto" href="#">Contacto</a>
+            <a class="nav-item nav-link login" href="#">Login</a>
+          </div>
+        </div>
+      </nav>
+    </header>
      <body>
        <div class="container use-ajax">
          <div class="row">
@@ -55,6 +77,10 @@ class TareasView
          </article>
        </div>
      </body>
+     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+     <script src="js/main.js"></script>
    </html>
    <?php
     }
