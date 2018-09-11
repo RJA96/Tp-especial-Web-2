e96e154f0fdf43478bb697867c87dd24f42bafb2
RJA96 <rodrigoaguilar96@gmail.com>
2018-09-11 00:47:20 -0300

    Empezado el route para mvc, creado htaccess, empezando a pasar todo a php

---
+++ route.php
@@ -0,0 +1,19 @@
+<?php
+require_once "php/contacto.php";
+//Para hacer mvc, creo el controlador, lo llamo y hago pretty url
+$controller = new TareasController();
+$partesURL = explode('/', $_GET['action']);
+
+if ($partesURL[0] == '') {
+  $controller->Home();
+}else {
+  if ($partesURL[0] == 'Contacto') {
+    $controller->MostrarContacto();
+  }elseif ($partesURL[0] == 'borrar') {
+    BorrarTarea($partesURL[1]);
+  }elseif ($partesURL[0] == 'completada') {
+      CompletarTarea($partesURL[1]);
+  }
+}
+
+ ?>
